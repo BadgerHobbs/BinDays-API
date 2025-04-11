96d2537f781c9b5338ba48b8a492fa5d2c652576
BadgerHobbs <andyjriggs@gmail.com>
2025-04-11 01:59:02 +0100

    Updated collector to use inheritdoc.

--- BinDays.Api.Collectors/Collectors/Councils/WestDevonBoroughCouncil.cs
+++ BinDays.Api.Collectors/Collectors/Councils/WestDevonBoroughCouncil.cs
@@ -8,37 +8,23 @@ namespace BinDays.Api.Collectors.Collectors.Councils
     /// </summary>
     internal sealed class WestDevonBoroughCouncil : CollectorBase, ICollector
     {
-        /// <summary>
-        /// Gets the name of the collector.
-        /// </summary>
+        /// <inheritdoc/>
         public override string Name => "West Devon Borough Council";
 
-        /// <summary>
-        /// Gets the website url of the collector.
-        /// </summary>
+        /// <inheritdoc/>
         public override Uri WebsiteUrl => new ("https://westdevon.fccenvironment.co.uk/mycollections");
 
-        /// <summary>
-        /// Gets the gov.uk id of the collector.
-        /// </summary>
+        /// <inheritdoc/>
         public override string GovUkId => "west-devon";
 
-        /// <summary>
-        /// Gets the addresses for a given postcode.
-        /// </summary>
-        /// <param name="postcode">The postcode to search for.</param>
-        /// <returns>A read only collection of addresses.</returns>
+        /// <inheritdoc/>
         public override async Task<ReadOnlyCollection<Address>> GetPostcodeAddresses(string postcode)
         {
             await Task.FromResult(string.Empty);
             throw new NotImplementedException("GetPostcodeAddresses not implemented.");
         }
 
-        /// <summary>
-        /// Gets the bin days for a given address.
-        /// </summary>
-        /// <param name="address">The address to search for.</param>
-        /// <returns>A read only collection of bin days.</returns>
+        /// <inheritdoc/>
         public override async Task<ReadOnlyCollection<BinDay>> GetAddressBinDays(Address address)
         {
             await Task.FromResult(string.Empty);
