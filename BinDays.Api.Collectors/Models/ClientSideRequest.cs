9428c8bdb4e3f579c9984fad7f35ead7db571a70
BadgerHobbs <andyjriggs@gmail.com>
2025-04-11 02:11:14 +0100

    Added models for client side request/response.

---
+++ BinDays.Api.Collectors/Models/ClientSideRequest.cs
@@ -0,0 +1,30 @@
+namespace BinDays.Api.Collectors.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Model which represents a HTTP request executed client-side.
+    /// </summary>
+    internal sealed class ClientSideRequest
+    {
+        /// <summary>
+        /// Gets the URL of the request.
+        /// </summary>
+        required public string Url { get; init; }
+
+        /// <summary>
+        /// Gets the HTTP method of the request.
+        /// </summary>
+        required public string Method { get; init; }
+
+        /// <summary>
+        /// Gets the headers of the request.
+        /// </summary>
+        required public Dictionary<string, string> Headers { get; init; }
+
+        /// <summary>
+        /// Gets the body of the request.
+        /// </summary>
+        required public string Body { get; init; }
+    }
+}
---
+++ BinDays.Api.Collectors/Models/ClientSideResponse.cs
@@ -0,0 +1,35 @@
+namespace BinDays.Api.Collectors.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Model which represents a HTTP response from a request executed client-side.
+    /// </summary>
+    internal sealed class ClientSideResponse
+    {
+        /// <summary>
+        /// Gets the HTTP status code of the response.
+        /// </summary>
+        required public int StatusCode { get; init; }
+
+        /// <summary>
+        /// Gets the headers of the response.
+        /// </summary>
+        required public Dictionary<string, string> Headers { get; init; }
+
+        /// <summary>
+        /// Gets the content of the response as a string.
+        /// </summary>
+        required public string Content { get; init; }
+
+        /// <summary>
+        /// Gets the reason phrase of the response.
+        /// </summary>
+        required public string ReasonPhrase { get; init; }
+
+        /// <summary>
+        /// Gets a value indicating whether the request was successful.
+        /// </summary>
+        public bool IsSuccessStatusCode => this.StatusCode >= 200 && this.StatusCode <= 299;
+    }
+}
