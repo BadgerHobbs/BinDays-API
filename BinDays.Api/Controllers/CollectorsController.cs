bad65b5e5f071ef424d329b65ab3d9177ed9da1a
BadgerHobbs <andyjriggs@gmail.com>
2025-04-12 15:38:47 +0100

    Added basic collectors controller and deleted samples.

---
+++ BinDays.Api/Controllers/CollectorsController.cs
@@ -0,0 +1,29 @@
+﻿namespace BinDays.Api.Controllers
+{
+    using BinDays.Api.Collectors.Collectors;
+    using BinDays.Api.Collectors.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Controller for managing collectors.
+    /// </summary>
+    [ApiController]
+    [Route("collectors")]
+    public class CollectorsController(CollectorService collectorService) : ControllerBase
+    {
+        /// <summary>
+        /// Service for returning specific or all collectors.
+        /// </summary>
+        private readonly CollectorService collectorService = collectorService;
+
+        /// <summary>
+        /// Gets all the collectors.
+        /// </summary>
+        /// <returns>A list of collectors.</returns>
+        [HttpGet]
+        public IEnumerable<ICollector> GetCollectors()
+        {
+            return this.collectorService.GetCollectors();
+        }
+    }
+}
