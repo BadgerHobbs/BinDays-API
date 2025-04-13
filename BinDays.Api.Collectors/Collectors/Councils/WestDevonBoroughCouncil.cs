cc55e27286793b730de25c09880e2adb656266d7
BadgerHobbs <andyjriggs@gmail.com>
2025-04-13 03:03:22 +0100

    Added logic to west devon borough council for getting bin days, and tested (Working!).

--- BinDays.Api.Collectors/Collectors/Councils/WestDevonBoroughCouncil.cs
+++ BinDays.Api.Collectors/Collectors/Councils/WestDevonBoroughCouncil.cs
@@ -3,13 +3,15 @@ namespace BinDays.Api.Collectors.Collectors.Councils
 	using BinDays.Api.Collectors.Models;
 	using BinDays.Api.Collectors.Utilities;
 	using System.Collections.ObjectModel;
+	using System.Globalization;
 	using System.Text.Json;
 	using System.Text.Json.Nodes;
+	using System.Text.RegularExpressions;
 
 	/// <summary>
 	/// Collector implementation for West Devon Borough Council.
 	/// </summary>
-	internal sealed class WestDevonBoroughCouncil : GovUkCollectorBase, ICollector
+	internal sealed partial class WestDevonBoroughCouncil : GovUkCollectorBase, ICollector
 	{
 		/// <inheritdoc/>
 		public string Name => "West Devon Borough Council";
@@ -19,6 +21,59 @@ namespace BinDays.Api.Collectors.Collectors.Councils
 
 		/// <inheritdoc/>
 		public override string GovUkId => "west-devon";
+
+		/// <summary>
+		/// Regex for the title within <h3> tags
+		/// </summary>
+		[GeneratedRegex(@"<h3.*?>\s*(.*?)\s*</h3>")]
+		private static partial Regex ServiceRegex();
+
+		/// <summary>
+		/// Regex for the date following specific text and within <b> tags
+		/// </summary>
+		[GeneratedRegex(@"Your next scheduled collection is\s*<b>\s*(.*?)\s*</b>")]
+		private static partial Regex DateRegex();
+
+		/// <summary>
+		/// The list of bin types for this collector.
+		/// </summary>
+		private readonly ReadOnlyCollection<Bin> binTypes = new List<Bin>()
+		{
+			new()
+			{
+				Name = "Plastic & Metal Recycling",
+				Colour = "White",
+				Keys = new List<string>() { "Recycling and Food" }.AsReadOnly(),
+				Type = "Bag",
+			},
+			new()
+			{
+				Name = "Paper, Glass, & Cartons Recycling",
+				Colour = "Green",
+				Keys = new List<string>() { "Recycling and Food" }.AsReadOnly(),
+				Type = "Box",
+			},
+			new()
+			{
+				Name = "Cardboard, Batteries, Ink, & Clothes Recycling",
+				Colour = "Green",
+				Keys = new List<string>() { "Recycling and Food" }.AsReadOnly(),
+				Type = "Box",
+			},
+			new()
+			{
+				Name = "Food Waste",
+				Colour = "Grey",
+				Keys = new List<string>() { "Recycling and Food" }.AsReadOnly(),
+				Type = "Bin",
+			},
+			new()
+			{
+				Name = "General Waste",
+				Colour = "Brown",
+				Keys = new List<string>() { "Refuse" }.AsReadOnly(),
+			},
+		}.AsReadOnly();
 
 		/// <inheritdoc/>
 		public GetAddressesResponse GetAddresses(string postcode, ClientSideResponse? clientSideResponse)
@@ -124,7 +179,113 @@ namespace BinDays.Api.Collectors.Collectors.Councils
 		/// <inheritdoc/>
 		public GetBinDaysResponse GetBinDays(Address address, ClientSideResponse? clientSideResponse)
 		{
-			throw new NotImplementedException("GetBinDays not implemented.");
+			// Prepare client-side request for getting session id
+			if (clientSideResponse == null)
+			{
+				// Prepare client-side request
+				var clientSideRequest = new ClientSideRequest()
+				{
+					RequestId = 1,
+					Url = "https://westdevon.fccenvironment.co.uk/mycollections",
+					Method = "GET",
+					Headers = [],
+					Body = string.Empty,
+				};
+
+				var getBinDaysResponse = new GetBinDaysResponse()
+				{
+					BinDays = null,
+					NextClientSideRequest = clientSideRequest
+				};
+
+				return getBinDaysResponse;
+			}
+			// Prepare client-side request for getting bin days
+			else if (clientSideResponse?.RequestId == 1)
+			{
+				// Get session id from response header
+				var sessionId = GetSessionId(clientSideResponse);
+
+				// Prepare client-side request
+				var requestBody = ProcessingUtilities.ConvertDictionaryToFormData(new Dictionary<string, string>()
+				{
+					{"fcc_session_token", sessionId},
+					{"uprn", address.Uid!},
+				});
+
+				var requestHeaders = new Dictionary<string, string>() {
+					{"cookie", $"fcc_session_cookie={sessionId}"},
+					{"x-requested-with", "XMLHttpRequest"},
+					{"content-type", "application/x-www-form-urlencoded; charset=UTF-8"},
+				};
+
+				var clientSideRequest = new ClientSideRequest()
+				{
+					RequestId = 2,
+					Url = "https://westdevon.fccenvironment.co.uk/ajaxprocessor/getcollectiondetails",
+					Method = "POST",
+					Headers = requestHeaders,
+					Body = requestBody,
+				};
+
+				var getBinDaysResponse = new GetBinDaysResponse()
+				{
+					BinDays = null,
+					NextClientSideRequest = clientSideRequest
+				};
+
+				return getBinDaysResponse;
+			}
+			// Process bin days from response
+			else if (clientSideResponse?.RequestId == 2)
+			{
+				// Parse response content as JSON object
+				var responseJson = JsonSerializer.Deserialize<JsonObject>(clientSideResponse.Content)!;
+				var binDaysJson = responseJson["binCollections"]!["tile"]!.AsArray();
+
+				// Iterate through each bin days html, and create a new bin days object
+				var binDays = new List<BinDay>();
+				foreach (var binDayHtml in binDaysJson)
+				{
+					// Using regex get the service and date
+					var service = ServiceRegex().Match(binDayHtml![0]!.ToString()).Groups[1].Value;
+					var collectionDate = DateRegex().Match(binDayHtml![0]!.ToString()).Groups[1].Value;
+
+					// Get matching bin types from the service using the keys
+					var binTypes = this.binTypes.Where(x => x.Keys.Any(y => service.Contains(y)));
+
+					// Parse the date (e.g. Tuesday, 15 April 2025) to date only
+					var date = DateTime.ParseExact(
+						collectionDate,
+						"dddd, dd MMMM yyyy",
+						CultureInfo.InvariantCulture,
+						DateTimeStyles.None
+					);
+
+					var binDay = new BinDay()
+					{
+						Date = DateOnly.FromDateTime(date),
+						Address = address,
+						Bins = binTypes.ToList().AsReadOnly()
+					};
+
+					binDays.Add(binDay);
+				}
+
+				// Merge the bin days
+				binDays = [.. ProcessingUtilities.MergeBinDays(binDays)];
+
+				var getBinDaysResponse = new GetBinDaysResponse()
+				{
+					BinDays = binDays.AsReadOnly(),
+					NextClientSideRequest = null
+				};
+
+				return getBinDaysResponse;
+			}
+
+			// Throw exception for invalid request
+			throw new InvalidOperationException($"Invalid client-side request.");
 		}
 
 		/// <summary>
--- BinDays.Api.Collectors/Utilities/ProcessingUtilities.cs
+++ BinDays.Api.Collectors/Utilities/ProcessingUtilities.cs
@@ -1,5 +1,7 @@
 namespace BinDays.Api.Collectors.Utilities
 {
+	using BinDays.Api.Collectors.Models;
+	using System.Collections.ObjectModel;
 	using System.Web;
 
 	/// <summary>
@@ -23,6 +25,43 @@ namespace BinDays.Api.Collectors.Utilities
 				$"{HttpUtility.UrlEncode(kvp.Key)}={HttpUtility.UrlEncode(kvp.Value)}"));
 
 			return formData;
+		}
+
+		/// <summary>
+		/// Merges a collection of BinDay objects, combining those with the same date.
+		/// </summary>
+		/// <param name="binDays">The collection of BinDay objects to merge.</param>
+		/// <returns>A read-only collection of merged BinDay objects.</returns>
+		public static ReadOnlyCollection<BinDay> MergeBinDays(IEnumerable<BinDay> binDays)
+		{
+			var mergedBinDays = new List<BinDay>();
+
+			// Group the input BinDay objects by their Date property.
+			var groupedBinDays = binDays.GroupBy(bd => bd.Date);
+
+			// Iterate through each group of BinDay objects with the same date.
+			foreach (var group in groupedBinDays)
+			{
+				// Initialize a list to store the merged Bins for the current date.
+				var mergedBins = new List<Bin>();
+
+				// Add all Bins from the current BinDay to the mergedBins list.
+				foreach (var binDay in group)
+				{
+					mergedBins.AddRange(binDay.Bins);
+				}
+
+				// Create a new BinDay object with the merged Bins, the common date, and the address from the first BinDay in the group.
+				var mergedBinDay = new BinDay
+				{
+					Date = group.Key,
+					Address = group.First().Address,
+					Bins = mergedBins.DistinctBy(b => b.Name).ToList().AsReadOnly()
+				};
+				mergedBinDays.Add(mergedBinDay);
+			}
+
+			return mergedBinDays.AsReadOnly();
 		}
 	}
 }
