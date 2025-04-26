dabb6dc6d6ccfc86f6ab32cdad80fcb5daa6e008
BadgerHobbs <andyjriggs@gmail.com>
2025-04-26 04:19:30 +0100

    Reverted back to working deployment.

--- Terraform/application.tf
+++ Terraform/application.tf
@@ -15,22 +15,7 @@ resource "digitalocean_droplet" "bindays_api" {
   region = "fra1"
   size   = "s-1vcpu-512mb-10gb"
   image  = "debian-12-x64"
-  ssh_keys = [digitalocean_ssh_key.bindays_api.id]
-
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.ipv4_address
-    }
-
-    inline = [
-      "apt update",
-      "apt upgrade -y",
-      "apt install -y docker.io"
-    ]
-  }
+  ssh_keys = [ digitalocean_ssh_key.bindays_api.id ]
 
   depends_on = [
     digitalocean_ssh_key.bindays_api
@@ -53,6 +38,8 @@ resource "null_resource" "bindays_api" {
     }
 
     inline = [
+      "apt update",
+      "apt install -y docker.io",
       "docker login -u ${var.ghcr_username} -p ${var.ghcr_access_token} ghcr.io/${var.ghcr_username}",
       "docker pull ghcr.io/${var.ghcr_username}/${var.docker_image}",
       "docker rm bindays-api -f",
