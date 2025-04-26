2d21b988720e61b32249551b312f075dfb33de59
BadgerHobbs <andyjriggs@gmail.com>
2025-04-26 02:15:36 +0100

    Added terraform for deploying to digital ocean.

---
+++ Terraform/variables.tf
@@ -0,0 +1,21 @@
+# DigitalOcean access token.
+variable "do_access_token" {
+    type = string
+    sensitive = true
+}
+
+# ghcr.io username.
+variable "ghcr_username" {
+    type = string
+}
+
+# ghcr.io access token (password).
+variable "ghcr_access_token" {
+    type = string
+    sensitive = true
+}
+
+# Docker image.
+variable "docker_image" {
+    type = string
+}
