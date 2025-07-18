bf8aed5647ad787e1d535f95c6fa4cdbeda1a491
BadgerHobbs <andyjriggs@gmail.com>
2025-07-18 02:53:03 +0100

    Added support for `CornwallCouncil`.

---
+++ BinDays.Api.Collectors/Collectors/Councils/CornwallCouncil.cs
@@ -0,0 +1,232 @@
+namespace BinDays.Api.Collectors.Collectors.Councils
+{
+    using BinDays.Api.Collectors.Models;
+    using BinDays.Api.Collectors.Utilities;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Collector implementation for Cornwall Council.
+    /// </summary>
+    internal sealed partial class CornwallCouncil : GovUkCollectorBase, ICollector
+    {
+        /// <inheritdoc/>
+        public string Name => "Cornwall Council";
+
+        /// <inheritdoc/>
+        public Uri WebsiteUrl => new("https://www.cornwall.gov.uk/my-area");
+
+        /// <inheritdoc/>
+        public override string GovUkId => "cornwall";
+
+        /// <summary>
+        /// The list of bin types for this collector.
+        /// </summary>
+        private readonly ReadOnlyCollection<Bin> binTypes = new List<Bin>()
+        {
+            new()
+            {
+                Name = "Food Waste",
+                Colour = "Green",
+                Keys = new List<string>() { "food" }.AsReadOnly(),
+            },
+            new()
+            {
+                Name = "Recycling",
+                Colour = "Orange",
+                Keys = new List<string>() { "recycling" }.AsReadOnly(),
+                Type = "Bag",
+            },
+            new()
+            {
+                Name = "Rubbish",
+                Colour = "Black",
+                Keys = new List<string>() { "rubbish" }.AsReadOnly(),
+            },
+            new()
+            {
+                Name = "Garden Waste",
+                Colour = "Brown",
+                Keys = new List<string>() { "garden" }.AsReadOnly(),
+            },
+        }.AsReadOnly();
+
+        /// <summary>
+        /// Regex for the addresses from the options elements.
+        /// </summary>
+        [GeneratedRegex(@"<option value=""(?<uid>\d+)"">(?<address>[\s\S]*?)<\/option>")]
+        private static partial Regex AddressesRegex();
+
+        /// <summary>
+        /// Regex for the bin days from the data elements.
+        /// </summary>
+        [GeneratedRegex(@"(?s)<div id=""(?<binId>[^""]+)-nom"" class=""collection.*?<span>[^<]+</span>\s*<span>[^<]+</span>\s*<span>(?<date>[^<]+)</span>")]
+        private static partial Regex BinDaysRegex();
+
+        /// <inheritdoc/>
+        public GetAddressesResponse GetAddresses(string postcode, ClientSideResponse? clientSideResponse)
+        {
+            // Prepare client-side request for getting addresses
+            if (clientSideResponse == null)
+            {
+                var requestUrl = $"https://www.cornwall.gov.uk/umbraco/surface/geo/MyAreaAddressList?postcode={postcode}";
+
+                var requestHeaders = new Dictionary<string, string>()
+                {
+                    { "X-Requested-With", "XMLHttpRequest" },
+                    { "User-Agent", Constants.UserAgent },
+                };
+
+                var clientSideRequest = new ClientSideRequest()
+                {
+                    RequestId = 1,
+                    Url = requestUrl,
+                    Method = "GET",
+                    Headers = requestHeaders,
+                    Body = string.Empty,
+                };
+
+                var getAddressesResponse = new GetAddressesResponse()
+                {
+                    Addresses = null,
+                    NextClientSideRequest = clientSideRequest
+                };
+
+                return getAddressesResponse;
+            }
+            // Process addresses from response
+            else if (clientSideResponse.RequestId == 1)
+            {
+                // Get addresses from response
+                var rawAddresses = AddressesRegex().Matches(clientSideResponse.Content);
+
+                var addresses = new List<Address>();
+                foreach (Match rawAddress in rawAddresses)
+                {
+                    var uid = rawAddress.Groups["uid"].Value;
+                    var addressText = rawAddress.Groups["address"].Value;
+
+                    // Decode HTML entities and take the first line of the address
+                    var decodedAddress = WebUtility.HtmlDecode(addressText);
+                    var property = decodedAddress.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries).FirstOrDefault()?.Trim();
+
+                    var address = new Address()
+                    {
+                        Property = property,
+                        Street = string.Empty,
+                        Town = string.Empty,
+                        Postcode = postcode,
+                        Uid = uid,
+                    };
+
+                    addresses.Add(address);
+                }
+
+                var getAddressesResponse = new GetAddressesResponse()
+                {
+                    Addresses = addresses.AsReadOnly(),
+                    NextClientSideRequest = null
+                };
+
+                return getAddressesResponse;
+            }
+
+            // Throw exception for invalid request
+            throw new InvalidOperationException("Invalid client-side request.");
+        }
+
+        /// <inheritdoc/>
+        public GetBinDaysResponse GetBinDays(Address address, ClientSideResponse? clientSideResponse)
+        {
+            // Prepare client-side request for getting bin days
+            if (clientSideResponse == null)
+            {
+                var requestUrl = $"https://www.cornwall.gov.uk/umbraco/surface/waste/MyCollectionDays?uprn={address.Uid}";
+
+                var requestHeaders = new Dictionary<string, string>()
+                {
+                    { "X-Requested-With", "XMLHttpRequest" },
+                    { "User-Agent", Constants.UserAgent },
+                };
+
+                var clientSideRequest = new ClientSideRequest()
+                {
+                    RequestId = 1,
+                    Url = requestUrl,
+                    Method = "GET",
+                    Headers = requestHeaders,
+                    Body = string.Empty,
+                };
+
+                var getBinDaysResponse = new GetBinDaysResponse()
+                {
+                    BinDays = null,
+                    NextClientSideRequest = clientSideRequest
+                };
+
+                return getBinDaysResponse;
+            }
+            // Process bin days from response
+            else if (clientSideResponse.RequestId == 1)
+            {
+                // Get bin days from response
+                var rawBinDays = BinDaysRegex().Matches(clientSideResponse.Content);
+
+                var binDays = new List<BinDay>();
+                foreach (Match rawBinDay in rawBinDays)
+                {
+                    var binId = rawBinDay.Groups["binId"].Value;
+                    var dateString = rawBinDay.Groups["date"].Value;
+
+                    // Parse date string (e.g., "18 Jul")
+                    var date = DateOnly.ParseExact(
+                        dateString,
+                        "d MMM",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None
+                    );
+
+                    // If the parsed date is in the past, assume it's for the next year
+                    if (date < DateOnly.FromDateTime(DateTime.Today))
+                    {
+                        date = date.AddYears(1);
+                    }
+
+                    // Get matching bin types from the bin ID using the keys
+                    var matchedBinTypes = binTypes.Where(x => x.Keys.Contains(binId));
+
+                    var binDay = new BinDay()
+                    {
+                        Date = date,
+                        Address = address,
+                        Bins = matchedBinTypes.ToList().AsReadOnly()
+                    };
+
+                    binDays.Add(binDay);
+                }
+
+                // Filter out bin days in the past
+                binDays = [.. ProcessingUtilities.GetFutureBinDays(binDays)];
+
+                // Merge bin days that fall on the same date
+                binDays = [.. ProcessingUtilities.MergeBinDays(binDays)];
+
+                var getBinDaysResponse = new GetBinDaysResponse()
+                {
+                    BinDays = binDays.AsReadOnly(),
+                    NextClientSideRequest = null
+                };
+
+                return getBinDaysResponse;
+            }
+
+            // Throw exception for invalid request
+            throw new InvalidOperationException("Invalid client-side request.");
+        }
+    }
+}
---
+++ BinDays.Api.IntegrationTests/Collectors/Councils/CornwallCouncilTests.cs
@@ -0,0 +1,37 @@
+namespace BinDays.Api.IntegrationTests.Collectors.Councils
+{
+	using BinDays.Api.Collectors.Collectors;
+	using BinDays.Api.Collectors.Collectors.Councils;
+	using BinDays.Api.Collectors.Services;
+	using BinDays.Api.IntegrationTests.Helpers;
+	using System.Threading.Tasks;
+	using Xunit;
+	using Xunit.Abstractions;
+
+	public class CornwallCouncilTests
+	{
+		private readonly IntegrationTestClient _client = new();
+		private static readonly ICollector _collector = new CornwallCouncil();
+		private readonly CollectorService _collectorService = new([_collector]);
+		private const string _postcode = "PL15 7DU";
+
+		private readonly ITestOutputHelper _outputHelper;
+
+		public CornwallCouncilTests(ITestOutputHelper outputHelper)
+		{
+			_outputHelper = outputHelper;
+		}
+
+		[Fact]
+		public async Task GetBinDaysTest()
+		{
+			await TestSteps.EndToEnd(
+				_client,
+				_collectorService,
+				_collector,
+				_postcode,
+				_outputHelper
+			);
+		}
+	}
+}
