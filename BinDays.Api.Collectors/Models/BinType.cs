b06af2bf2a04936766906c7d479b487b6758d7c7
BadgerHobbs <andyjriggs@gmail.com>
2025-10-10 00:41:35 +0100

    Update spacing.

--- BinDays.Api.Collectors/Models/BinType.cs
+++ BinDays.Api.Collectors/Models/BinType.cs
@@ -9,22 +9,27 @@ namespace BinDays.Api.Collectors.Models
         /// A standard wheeled bin.
         /// </summary>
         Bin,
+
         /// <summary>
         /// A box for recycling.
         /// </summary>
         Box,
+
         /// <summary>
         /// A bag for waste or recycling.
         /// </summary>
         Bag,
+
         /// <summary>
         /// A small caddy, typically for food waste.
         /// </summary>
         Caddy,
+
         /// <summary>
         /// A sack for waste or recycling.
         /// </summary>
         Sack,
+
         /// <summary>
         /// A generic container for waste.
         /// </summary>
