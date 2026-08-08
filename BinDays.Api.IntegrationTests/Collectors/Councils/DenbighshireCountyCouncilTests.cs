489c53e6a2d07a2d18be8b0d8ec6d2374bb91748
BadgerHobbs <andyjriggs@gmail.com>
2026-08-08 23:54:57 +0100

    Add pinned-uid regression test case
    Brings the collector in line with the pinned-uid test pattern now
    used across all other councils, added to main after this branch was
    created.

--- BinDays.Api.IntegrationTests/Collectors/Councils/DenbighshireCountyCouncilTests.cs
+++ BinDays.Api.IntegrationTests/Collectors/Councils/DenbighshireCountyCouncilTests.cs
@@ -20,13 +20,16 @@ public class DenbighshireCountyCouncilTests
 
 	[Theory]
 	[InlineData("LL19 8AD")]
-	public async Task GetBinDaysTest(string postcode)
+	[InlineData("LL19 8AD", "100100189198", 1)]
+	public async Task GetBinDaysTest(string postcode, string? pinnedUid = null, int? pinnedVersion = null)
 	{
 		await TestSteps.EndToEnd(
 			_client,
 			postcode,
 			_govUkId,
-			_outputHelper
+			_outputHelper,
+			pinnedUid: pinnedUid,
+			pinnedVersion: pinnedVersion
 		);
 	}
 }
